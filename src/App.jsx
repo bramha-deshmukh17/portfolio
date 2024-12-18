d1b62d25cb4e4c884c151af1b5327afa77d60537
bramha-deshmukh17 <bramhadeshmukh3@gmail.com>
2024-12-18 13:13:58 +0530

    Added Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './App.css';
+import './index.css';
 
 const App = () => {
   return (
