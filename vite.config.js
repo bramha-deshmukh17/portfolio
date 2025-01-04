540b98b2d0fcc291cf3720bb27df4a9ef44356ad
bramha-deshmukh17 <bramhadeshmukh3@gmail.com>
2025-01-04 19:53:55 +0530

    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import { ghPages } from 'vite-plugin-gh-pages'; // Ensure this matches the plugi
 export default defineConfig({
   plugins: [react(), ghPages()],
   base: '/portfolio/', // Replace <repository-name> with your GitHub repository name
+  server: {
+    host: '0.0.0.0',
+    port: 3000, // or any port you prefer
+  },
 });
